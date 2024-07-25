0947b285c2f57bbfb292145d351042657d47b57c
Lucas Kissmann <lucaskissmann@gmail.com>
2024-07-25 08:17:04 -0300

    fix: spring annotations on controller

--- pix-producer/src/main/java/br/com/pix/controllers/PixController.java
+++ pix-producer/src/main/java/br/com/pix/controllers/PixController.java
@@ -2,12 +2,19 @@ package br.com.pix.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.pix.modules.pix.dtos.PixDTO;
 import br.com.pix.services.PixService;
+import lombok.RequiredArgsConstructor;
 
+@RestController
+@RequestMapping("/pix")
+@RequiredArgsConstructor
 public class PixController {
-	private PixService pixService;
+	
+    private final PixService pixService;
 
     @PostMapping
     public PixDTO create(@RequestBody PixDTO pixDTO) {
